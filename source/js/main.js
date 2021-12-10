ca31cf23a5feed0688eb4645fa7d7948cb859ea6
Faceoff88 <feizov88@gmail.com>
2021-12-10 13:12:00 +0300

    Добавляет стили для мобильных секциям header and intro

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,14 @@
 var menuButton= document.querySelector('.menu__toggle-nav');
 var menu = document.querySelector('.nav');
 var main = document.querySelector('.main');
+var footer = document.querySelector('.page-footer');
 var menuBurger = document.querySelector('.menu-burger');
 var closeCross = document.querySelector('.close-cross');
 
 menuButton.addEventListener('click', function(evt) {
     menu.classList.toggle('nav-active');
     main.classList.toggle('main-active');
+    footer.classList.toggle('main-active');
     menuBurger.classList.toggle('visually-hidden');
     closeCross.classList.toggle('visually-hidden');
 })
