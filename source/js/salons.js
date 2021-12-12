eb84688ec08d570609a2ff6484e4887d31c772f9
Faceoff88 <feizov88@gmail.com>
2021-12-12 14:42:31 +0300

    Добавляет адаптивные стили странице с Салонами

---
+++ source/js/salons.js
@@ -0,0 +1,65 @@
+var moscowButton = document.querySelector('.moscow__title');
+var moscowList = document.querySelector('.moscow__list');
+var mosoblButton = document.querySelector('.mosobl__title');
+var mosoblList = document.querySelector('.mosobl__list');
+var spbButton = document.querySelector('.spb__title');
+var spbList = document.querySelector('.spb__list');
+var nnButton = document.querySelector('.nn__title');
+var nnList = document.querySelector('.nn__list');
+var novosibButton = document.querySelector('.novosib__title');
+var novosibList = document.querySelector('.novosib__list');
+var voronezhButton = document.querySelector('.voronezh__title');
+var voronezhList = document.querySelector('.voronezh__list');
+var samaraButton = document.querySelector('.samara__title');
+var samaraList = document.querySelector('.samara__list');
+var yaroslavlButton = document.querySelector('.yaroslavl__title');
+var yaroslavlList = document.querySelector('.yaroslavl__list');
+var ufaButton = document.querySelector('.ufa__title');
+var ufaList = document.querySelector('.ufa__list');
+
+moscowButton.addEventListener('click', function() {
+    moscowButton.classList.toggle('button-active');
+    moscowList.classList.toggle('salons-active');
+})
+
+mosoblButton.addEventListener('click', function() {
+    mosoblButton.classList.toggle('button-active');
+    mosoblList.classList.toggle('salons-active');
+})
+
+spbButton.addEventListener('click', function() {
+    spbButton.classList.toggle('button-active');
+    spbList.classList.toggle('salons-active');
+})
+
+nnButton.addEventListener('click', function() {
+    nnButton.classList.toggle('button-active');
+    nnList.classList.toggle('salons-active');
+})
+
+novosibButton.addEventListener('click', function() {
+    novosibButton.classList.toggle('button-active');
+    novosibList.classList.toggle('salons-active');
+})
+
+voronezhButton.addEventListener('click', function() {
+    voronezhButton.classList.toggle('button-active');
+    voronezhList.classList.toggle('salons-active');
+})
+
+samaraButton.addEventListener('click', function() {
+    samaraButton.classList.toggle('button-active');
+    samaraList.classList.toggle('salons-active');
+})
+
+yaroslavlButton.addEventListener('click', function() {
+    yaroslavlButton.classList.toggle('button-active');
+    yaroslavlList.classList.toggle('salons-active');
+})
+
+ufaButton.addEventListener('click', function() {
+    ufaButton.classList.toggle('button-active');
+    ufaList.classList.toggle('salons-active');
+})
+
+
